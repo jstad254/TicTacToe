9cc73d8b2db9dfc271a7d398b6613250ee528ccc
jstad254 <jfollstad@olympicproductsinc.com>
2020-09-14 18:30:27 -0400

    first commit

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string TopLeft = " ";
+            string TopCenter = " ";
+            string TopRight = " ";
+            string MidLeft = " ";
+            string MidCenter = " ";
+            string MidRight = " ";
+            string BottomLeft = " ";
+            string BottomCenter = " ";
+            string BottomRight = " ";
+
+            Console.WriteLine("");
+            Console.WriteLine($" " + TopLeft + " | " + TopCenter + " | " + TopRight + " ");
+            Console.WriteLine("-----------");
+            Console.WriteLine($" " + MidLeft + " | " + MidCenter + " | " + MidRight + " ");
+            Console.WriteLine("-----------");
+            Console.WriteLine($" " + BottomLeft + " | " + BottomCenter + " | " + BottomRight + " ");
+
+            Console.ReadLine();
+        }
+    }
+}
